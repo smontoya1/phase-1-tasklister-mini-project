c4d354706837e19b83231e5f105a5c3df663407d
Shaun <smontoya0009@gmail.com>
2025-04-13 12:17:51 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const form = document.getElementById("create-task-form");
+const taskDescription = document.getElementById("new-task-description");
+const taskList = document.getElementById("tasks");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newTask = document.createElement("li");
+  newTask.innerText = taskDescription.value;
+
+  taskList.appendChild(newTask);
 });
